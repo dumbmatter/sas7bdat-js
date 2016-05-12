ca9d41ca61ef1541e9755a09d9e02a71507c8206
Jeremy Scheff <jdscheff@gmail.com>
2016-05-12 09:15:53 -0400

    Reorganization, CSV files

--- test/data/README.md
+++ test/data/README.md
@@ -1,2 +1,3 @@
-Most files came from https://github.com/BioStatMatt/sas7bdat but some came from
-https://github.com/hadley/haven and hopefully others will be added too.
+Most .sas7bdat files came from https://github.com/BioStatMatt/sas7bdat but some came from https://github.com/hadley/haven and hopefully others will be added too.
+
+CSV files were converted from sas7bdat by StatTransfer13.
--- test/index.js
+++ test/index.js
@@ -5,12 +5,8 @@ const sas7bdat = require('../index');
 // Smoke tests - run on various data files and see if there is an error
 describe('smoke tests', function () {
     this.timeout(20000);
-    const filenames = fs.readdirSync(path.join(__dirname, 'data')).filter(filename => filename.includes('sas7bdat'));
+    const filenames = fs.readdirSync(path.join(__dirname, 'data/sas7bdat')).filter(filename => filename.includes('sas7bdat'));
     for (const filename of filenames) {
-        if (filename === 'osteo_analysis_data.sas7bdat') {
-            it.skip(filename, () => sas7bdat.parse(path.join(__dirname, 'data', filename)));
-        } else {
-            it(filename, () => sas7bdat.parse(path.join(__dirname, 'data', filename)));
-        }
+        it(filename, () => sas7bdat.parse(path.join(__dirname, 'data/sas7bdat', filename)));
     }
 });
