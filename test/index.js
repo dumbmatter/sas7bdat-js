68e28939ed19c77d3a514ca0eb39772d541af8a5
Jeremy Scheff <jdscheff@gmail.com>
2016-05-12 09:34:01 -0400

    Some CSVs exactly match StatTransfer...

--- test/index.js
+++ test/index.js
@@ -1,12 +1,38 @@
+const assert = require('assert');
+const denodeify = require('denodeify');
 const fs = require('fs');
 const path = require('path');
+const stringify = require('csv-stringify');
 const sas7bdat = require('../index');
 
+const stringifyAsync = denodeify(stringify);
+
+const sasFilenames = fs.readdirSync(path.join(__dirname, 'data/sas7bdat'))
+    .filter(filename => filename.includes('sas7bdat'));
+
 // Smoke tests - run on various data files and see if there is an error
-describe('smoke tests', function () {
+describe('Smoke tests', function () {
     this.timeout(20000);
-    const filenames = fs.readdirSync(path.join(__dirname, 'data/sas7bdat')).filter(filename => filename.includes('sas7bdat'));
-    for (const filename of filenames) {
+    for (const filename of sasFilenames) {
         it(filename, () => sas7bdat.parse(path.join(__dirname, 'data/sas7bdat', filename)));
+    }
+});
+
+describe.only('Compare to StatTransfer CSV export', function () {
+    this.timeout(20000);
+    for (const filename of sasFilenames) {
+        it(filename, async () => {
+            const data = await sas7bdat.parse(path.join(__dirname, 'data/sas7bdat', filename));
+            const options = {
+                columns: data.cols.map(col => col.name),
+                header: true,
+                quotedString: true
+            };
+            const csv = await stringifyAsync(data.rows, options);
+            const filename2 = filename.replace('sas7bdat', 'csv');
+            const csv2 = fs.readFileSync(path.join(__dirname, 'data/csv', filename2), 'utf8');
+
+            assert.equal(csv, csv2);
+        });
     }
 });
